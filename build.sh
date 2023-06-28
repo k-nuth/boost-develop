93eee0aa6fd0415077c08b2704c430a0f30990b5
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-06-28 10:18:34 +0200

    version cci.20230627

---
+++ build.sh
@@ -0,0 +1,9 @@
+rm -rf build
+rm conan.lock
+
+export VERSION="cci.20230627"
+
+conan lock create conanfile.py --version=$VERSION --update
+conan lock create conanfile.py --version $VERSION --lockfile=conan.lock --lockfile-out=build/conan.lock
+conan create conanfile.py --version $VERSION --lockfile=build/conan.lock
+conan upload boost-develop/$VERSION -r kth --only-recipe
